0e29f294c470cdecafe84c9866dfaeec59c875c7
Maya <Maya@DESKTOP-L92MG7C>
2024-10-11 21:11:26 -0700

    Added requirements file.

--- eyetracking.py
+++ eyetracking.py
@@ -1,1 +1,14 @@
-# New file
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+image = cv2.imread('TestImages/brown_eyes.jpeg')
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+cv2.imshow('Faces', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
