2a0615f4b7d25406992bd84acd2cc151a2b63ed9
Maya <Maya@DESKTOP-L92MG7C>
2024-10-12 22:54:07 -0700

    Started integration.

--- calibrate_frame.py
+++ calibrate_frame.py
@@ -8,13 +8,13 @@ from GazeTracking.gaze_tracking import GazeTracking
 import tkinter as tk
 import ttkbootstrap as ttk
 import threading
+import pyautogui
+import numpy as np
+import time
 
-# xWidth, yHeight = pyautogui.size()
-xcoef = []
-ycoef = []
+global xcoef, ycoef
 
-
-class CalibrateScreen(tk.Frame):
+class CalibrateScreen(tk.Frame, gazeObject, camObject, cellWidth, cellHeight):
     def __init__(self, window, app):
         self.window = window
         self.window.attributes("-fullscreen", True)
@@ -31,13 +31,19 @@ class CalibrateScreen(tk.Frame):
         self.width = window.winfo_screenwidth()
         self.height = window.winfo_screenheight()
 
+        # Tranformation Function Paramters
+        self.cellWidth = cellWidth
+        self.cellHeight = cellHeight
+        self.screenWidth = pyautogui.size()[0]
+        self.screenHeight = pyautogui.size()[1]
+
         # Instructions message
         self.display_instructions()
 
         # Corner dots information
-        self.dot_size = 20
-        self.current_corner = 0
-        self.corners = [
+        self.dotSize = 20
+        self.currentPosition = 0
+        self.dotPositions = [
             (0.5, 0.05),  # Top-left corner
             (0.5, 0.25),
             (0.5, 0.5),  # Center
@@ -49,24 +55,24 @@ class CalibrateScreen(tk.Frame):
             (0.75, 0.5),
             (0.95, 0.5),  # Bottom-right corner
         ]
-        self.eyeData = [
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-        ]
+        self.eyeData = [[] for _ in range(len(self.dotPositions))]
         self.dotShowing = False
         self.initialize_dots_as_circles()
 
+        # Initialize gaze tracking
+        self.gaze = gazeObject
+        self.webcam = camObject
+
         # Create a thread to track the gaze
+        self.collectData = False
+        self.calibrate = True
         self.gaze_thread = threading.Thread(target=self.track_gaze, daemon=True)
         self.gaze_thread.start()
+        self.lock = threading.Lock()
+
+        # Timing variables
+        self.timeDelay = 500
+        self.timeCollection = 3000
 
     def display_instructions(self):
         """
@@ -86,20 +92,12 @@ class CalibrateScreen(tk.Frame):
         )
         labelBottom.place(x=100, y=100)
 
-        # self.message = self.canvas.create_text(
-        #     self.width // 3,
-        #     self.height // 6,
-        #     text="Look at the red dot for 2 seconds. \nPress space to begin timer.",
-        #     font=("Arial", 24),
-        #     fill="white",
-        # )
-
     def create_dot(self, corner_index, fill, outline=""):
         """
         Creates a dot at the specified location (corner_index) on the screen with
         the specified fill and outline.
         """
-        corner = self.corners[corner_index]
+        corner = self.dotPositions[corner_index]
         x = self.width * corner[0]
         y = self.height * corner[1]
         self.canvas.create_oval(
@@ -121,63 +119,76 @@ class CalibrateScreen(tk.Frame):
         self.create_dot(0, fill="red")
 
         # other dots empty circles
-        for i in range(1, len(self.corners)):
+        for i in range(1, len(self.dotPositions)):
             self.create_dot(i, fill="", outline="black")
 
     def dot_on(self, event=None):
         """
         Starts measuring at current dot and signals for gaze tracking.
         """
-        if self.current_corner == len(self.corners):
+        if self.currentPosition == len(self.dotPositions):
             self.window.unbind("<space>")
             return
         # current position on the screen
-        x = self.width * self.corners[self.current_corner][0]
-        y = self.height * self.corners[self.current_corner][1]
+        x = self.width * self.dotPositions[self.currentPosition][0]
+        y = self.height * self.dotPositions[self.currentPosition][1]
 
         # Make dot yellow and start gaze tracking collection
-        self.create_dot(self.current_corner, fill="yellow")
+        self.create_dot(self.currentPosition, fill="yellow")
+        self.collectData = True
 
         self.dotShowing = True  # turns on gaze tracking
         self.window.after(2000, self.dot_off)
 
-        self.current_corner += 1
+        
 
     def dot_off(self, event=None):
         """
         After 2 seconds, turns off measuring at dot and stops gaze tracking.
         Changes dot to green color and next dot to red color.
         """
-        self.dotShowing = False  # turns off gaze tracking
-        self.create_dot(self.current_corner - 1, fill="green")
+        with self.lock:
+            if len(self.eyeData[self.currentPosition]) > 10:
+                self.collectData = False
 
-        # make next one red
-        if self.current_corner != len(self.corners):
-            self.create_dot(self.current_corner, fill="red")
+                self.currentPosition += 1
+                self.dotShowing = False  # turns off gaze tracking
+                self.create_dot(self.currentPosition - 1, fill="green")
+
+                # make next one red
+                if self.currentPosition != len(self.dotPositions):
+                    self.create_dot(self.currentPosition, fill="red")
+                else:
+                    self.finish_calibration()
+            else:
+                print("not enough data collected, waiting...")
+                self.window.after(100, self.dot_off)
 
     def track_gaze(self):
         """
         Tracks the gaze of the user and records the pupil position data.
         """
-        gaze = GazeTracking()
-        webcam = cv2.VideoCapture(0)
+    
+        while(self.calibrate):
+            if (self.collectData):
+                _, frame = self.webcam.read()
+                self.gaze.refresh(frame)
 
-        while self.current_corner < len(self.corners):
-            _, frame = webcam.read()
-            gaze.refresh(frame)  # processing step
+                left_pupil = self.gaze.pupil_left_coords()
+                right_pupil = self.gaze.pupil_right_coords()
 
-            left_pupil = gaze.pupil_left_coords()
-            right_pupil = gaze.pupil_right_coords()
-
-            # Store the gaze data only when the dot is displayed
-            if self.dotShowing:
-                if left_pupil and right_pupil:
+                if left_pupil and right_pupil and self.currentPosition < len(self.dotPositions):
                     # Record the average gaze position
-                    avgGaze = self.avgPosition(left_pupil, right_pupil)
-                    self.eyeData[self.current_corner].append(avgGaze)
+                    avgGaze = [(left_pupil[0] + right_pupil[0]) / 2, (left_pupil[1] + right_pupil[1]) / 2]
+                    with self.lock:
+                        self.eyeData[self.currentPosition].append(avgGaze)
+                        # print(self.eyeData)
+            else:
+                time.sleep(0.1)
+
 
         # when all corners have been calibrated, calculate the coefficients
-        if self.current_corner == len(self.corners):
+        if self.currentPosition == len(self.dotPositions):
             # clear screen
             for widget in self.app.root.winfo_children():
                 widget.destroy()
@@ -188,5 +199,43 @@ class CalibrateScreen(tk.Frame):
             self.window.geometry("800x600")
             ttk.Label(self.app.root, text="Calibration complete").pack(pady=20)
 
-    def avgPosition(self, leftEye, rightEye):
-        return [(leftEye[0] + rightEye[0]) / 2, (leftEye[1] + rightEye[1]) / 2]
+    def exit_fullscreen(self, event=None):
+        # stop thread
+        self.calibrate = False
+        if self.gaze_thread.is_alive():
+            self.gaze_thread.join()
+
+        # close calibration window
+        self.window.attributes('-fullscreen', False)
+        self.window.destroy()
+
+        # mappingfunction
+        global xcoef, ycoef
+        xcoef, ycoef = self.calculateFunctionGrid()
+
+    def calculateFunctionGrid(self):
+        # setup grid 
+        gridWidth = self.screenWidth // self.cellWidth
+        gridHeight = self.screenHeight // self.cellHeight
+
+        # stack the eye data for each dot 
+        data = np.vstack(self.eyeData)
+        x_eye = data[:,0]
+        y_eye = x_eye = data[:,1]
+
+        # stack the targets data
+        targets = []
+        for i in range(len(self.dotPositions)):
+            xPix = int(self.dotPositions[i][0]*gridWidth) # x
+            yPix = int(self.dotPositions[i][1]*gridHeight) # y
+            for j in range(len(self.eyeData[i])):
+                targets.append([xPix, yPix])
+        targets = np.array(targets)
+        x_pixel = np.array(targets[:,0])
+        y_pixel = np.array(targets[:,1])
+
+        # calculate polyfit
+        ycoeffs = np.polyfit(y_eye, y_pixel, 2)
+        xcoeffs = np.polyfit(x_eye, x_pixel, 2)
+        print(f"{ycoeffs}, {xcoeffs}")
+        return [xcoeffs, ycoeffs]
